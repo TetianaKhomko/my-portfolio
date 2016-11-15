e0484e682d4dfbfe5d7fe728fdbd5a7a53af1d96
Tetiana Khomko <arnelli.tt@gmail.com>
2016-11-15 00:24:41 +0000

    Next changes

---
+++ js/form.js
@@ -0,0 +1,22 @@
+
+$(function() {
+    $('#send').click(function(e) {
+        e.preventDefault();
+        $.ajax({
+                url: "https://formspree.io/arnelli.tt@gmail.com",
+                method: "POST",
+                data: {
+                    userName: $('#userName').val(),
+                    age: $('#age').val(),
+                    comment: $('#comment').val()
+                },
+                dataType: "json"
+            })
+            .done(function() {
+                $('form').html('<h1>Thank you!</h1>')
+            })
+            .fail(function() {
+                $('form').html('<h1>Error</h1>')
+            });
+    });
+});
